4de6272d338ecb9c8bca873aa0749521658d6891
Rishabh Patel <s4021464@student.rmit.edu.au>
2023-12-11 13:44:18 +0400

    edited code to present sequence

--- src/05/Challenge/ch5_factorial_seq.cpp
+++ src/05/Challenge/ch5_factorial_seq.cpp
@@ -19,18 +19,32 @@ int print_factorials(int n){
     // Write your code here
     int factorial = 1;
 
+    std::cout << n << "!: ";
+
     for (int i = 1; i < n+1; i++) {
+        std::cout << factorial << " ";
         factorial *=i;
 
 
     }
+
+    std::cout << std::endl << std::endl << std::flush;
+
+    std::cout << "The factorial of " << n << ": " << factorial << std::endl << std::endl << std::flush;
 
     return factorial;
 }
 
 // Main function
 int main(){
-    
-    std::cout << print_factorials(10) << std::endl << std::endl << std::flush;
+    int n;
+    std::cout << "Enter a positive integer: " ;
+    std::cin >> n;
+
+    std::cout << std::endl << std::endl << std::flush;
+
+    print_factorials(n);
+
+    std::cout << std::endl << std::endl << std::flush;
     return 0;
 }
