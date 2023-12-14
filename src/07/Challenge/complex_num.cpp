70822b4e1598149ac5e54ccf15b13f19eb138f59
Rishabh Patel <s4021464@student.rmit.edu.au>
2023-12-14 12:34:48 +0400

    wrote code to perform basic math functions for complex numbers

--- src/07/Challenge/complex_num.cpp
+++ src/07/Challenge/complex_num.cpp
@@ -2,32 +2,56 @@
 
 complex_num::complex_num(double r, double i){
     // Write your code here
+    real = r;
+    imaginary = i;
 }
 
 complex_num complex_num::operator +(complex_num w){
     complex_num z;
     // Write your code here
+
+    z.real = real + w.real;
+    z.imaginary = imaginary + w.imaginary;
+
     return z;
 }
 
 complex_num complex_num::operator -(complex_num w){
     complex_num z;
     // Write your code here
+
+    z.real = real - w.real;
+    z.imaginary = imaginary - w.imaginary;
+
     return z;
 }
 
 complex_num complex_num::operator *(complex_num w){
     complex_num z;
     // Write your code here
+
+    z.real = (real * w.real) - (imaginary * w.imaginary);
+    z.imaginary = (real * w.imaginary) + (imaginary * w.real);
+
     return z;
 }
 
 complex_num complex_num::operator /(complex_num w){
     complex_num z;
     // Write your code here
+
+    z.real = ((real * w.real) + (imaginary * w.imaginary)) / ((w.real * w.real) + (w.imaginary * w.imaginary));
     return z;
 }
 
 void complex_num::print(std::ostream &os){
     // Write your code here
+    if (imaginary >= 0.0) {
+        os << real << " + i" << imaginary;
+    } else {
+        os << real << " - i" << imaginary * -1;
+    }
+
+    os << std::flush;
+     
 }
