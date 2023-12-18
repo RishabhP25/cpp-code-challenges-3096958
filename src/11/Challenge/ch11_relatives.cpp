0209ad971823db256a57991c5d16d72c1365d394
Rishabh Patel <s4021464@student.rmit.edu.au>
2023-12-18 14:14:55 +0400

    created code to read and find names with similar surnames from a csv file

--- src/11/Challenge/ch11_relatives.cpp
+++ src/11/Challenge/ch11_relatives.cpp
@@ -11,16 +11,30 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <bits/stdc++.h>
 
 // Finding Relatives, main()
 // Summary: This application displays the first pair of possible relatives from a list of names in a CSV file.
 int main(){
     std::fstream file ("names.csv", std::ios::in);
+    
     // Read the CSV file.
+    std::vector<std::string> names;
     if(file.is_open()){
 
         // Write your code here
-    
+        std::string line ;
+
+        while (std::getline(file, line)) {
+            std::stringstream ss(line);
+            std::string name;
+
+            while (std::getline(ss, name, ',')) {
+                names.push_back(name);
+            }
+        } 
+
+
         file.close();
     }
     else{
@@ -29,8 +43,40 @@ int main(){
     }
 
     // Write your code here
+    std::vector<std::string> surnames;
+    for (const auto& name : names) {
 
-    std::cout << "No relatives found.\n\n" << std::flush;    
-    return 0;
+        
+        std::stringstream ss(name);
+        std::string firstName, surname;
+
+        ss >> firstName;
+
+        ss >> surname;
+        surnames.push_back(surname);
+
+    }
+
+    std::string check = surnames[0];
+    int index = 0;
+    int indexF;
+    for (int i=1; i < surnames.size(); i++) {
+        
+        auto it = find(surnames.begin() + i, surnames.end(), check);
+
+        if (it != surnames.end()) {
+            indexF = it - surnames.begin();
+            break;
+            
+        } else {
+            index++;
+            check = surnames[index];
+        }
+    } 
+
+    std::cout << "No relatives found.\n\n" << std::flush;  
+
+    std::cout << "Relatives with similar surnames: " << names[index] << " and " << names[indexF] << std::endl << std::endl << std::flush;  
+    return 0; 
 }
  
